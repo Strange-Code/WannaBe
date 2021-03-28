08841148809f7e50b15a2dd1290fa24fcdfb7b82
strngrwrld <gustavo-1477@hotmail.com>
2021-03-27 19:39:40 -0500

    Implementa randoom responsive

--- assets/js/random.js
+++ assets/js/random.js
@@ -1,8 +1,18 @@
 $("#random").hover(function() {
 
+    //var altura = screen.height;
+    var ancho = screen.width;
+    var randomAncho = 1500;
+    //console.log('ALTO' + altura)
+    console.log('ANCHO' + ancho);
+    console.log('Math.random()' + Math.random());
+    if (ancho < 980) {
+        randomAncho = 200;
+    }
+
     $("#random").addClass("random");
     $("#random").css("top", `${Math.random() * 700}px`);
-    $("#random").css("right", `${Math.random() * 1500}px`);
+    $("#random").css("right", `${Math.random() * randomAncho}px`);
 
 });
 
