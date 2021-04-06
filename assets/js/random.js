9326ea5c2bbd8b77b9100f81fe8bd58cff6ca433
strngrwrld <gustavo-1477@hotmail.com>
2021-04-05 21:31:42 -0500

    Se implementa estilos

--- assets/js/random.js
+++ assets/js/random.js
@@ -10,19 +10,47 @@ $("#random").hover(function() {
         randomAncho = 200;
     }
 
+    //$("#random").innerHTML = "Si";
+
     $("#random").addClass("random");
     $("#random").css("top", `${Math.random() * 700}px`);
     $("#random").css("right", `${Math.random() * randomAncho}px`);
 
-});
 
+    var x = Math.floor((Math.random() * 10) + 1);
+    var audio = document.getElementById('audio');
+
+    if (x % 2 == 0) {
+        audio.src = 'assets/audio/casi1.mp3';
+    } else {
+        audio.src = 'assets/audio/casi2.mp3';
+    }
+
+    /*
+    switch (foo) {
+        case 1:
+            audio.src = 'assets/audio/acepto.mp3';
+        default:
+            audio.src = 'assets/audio/acepto.mp3';
+    }
+    */
+    audio.load();
+    audio.play();
+
+});
 
 $("#yes").click(function() {
 
     $("#message").css("display", 'block');
+    var audio = document.getElementById('audio');
+    audio.src = 'assets/audio/acepto.mp3';
+    audio.load();
+    audio.play();
 });
 
 $("#close").click(function() {
+
+
 
     $("#message").css("display", 'none');
 });
